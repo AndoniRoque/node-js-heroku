d942f6d14684fb8820a33f8c8f0249c9179a68de
Sebastian Gañan <sganan81@gmail.com>
2022-10-13 15:35:26 -0300

    PR_devolucion

--- controllers/nasa.js
+++ controllers/nasa.js
@@ -3,21 +3,44 @@ const axios = require('axios').default;
 const key = process.env.API_KEY;
 
 const getPictures = (req = request, res = response) => {
+    //Estandarizar formatos de respuesta/salida
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=${key}&count=50`)
         .then ((response)=> {
-            res.status(200).json(response.data)
+            const {
+                status, 
+                statusText,               
+            } = response;
+             
+            //res.status(200).json(response.data)
+            //Ej de Estandarización de formato de salida/respuesta
+            res.status(status).json({                
+                'status':statusText,                
+                data:response.data
+            });
+            
         })
         .catch((err) => {
-            res.status(404);
+            //res.status(404);
+            //Ej de Estandarización de formato de salida/respuesta
+            const {message = '', code = ''} = err;
+            res.status(400).json({
+                status:'error',
+                message,
+                code,
+            });
         })
 }
 
 const getPictureByDate = (req = request, res = response) => {
+    //Estandarizar formatos de respuesta/salida
+    //No utilizar los query params directamente en la url, realizar desestructuración primero y utilizar la constante en la url
+    //EJ: const {date} = req.params; 
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${req.params.date}`)
         .then((response) => {
             res.status(200).json(response.data);
         })
-        .catch((err) => {
+        .catch((err) => {            
+            //Verificar si se peude obtener el codigo de error desde la api de lan NASA
             res.status(406).json({
                 msg: 'Al parecer las fechas ingresadas no dan un resultado aceptado.\
                 Por favor asegúrese de respetar el formato: YYYY-MM-DD.'
@@ -29,11 +52,14 @@ const getPicturesByDateRange = (req = request, res = response)  => {
     console.log(req.query.start_date);
     console.log(req.query.end_date);
     const {start_date, end_date} = req.query
+    //Estandarizar formatos de respuesta/salida           
+    //Utilizar las constantes desesctructuradas en la url : start_date, end_date    
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=${key}&start_date=${req.query.start_date}&end_date=${req.query.end_date}`)     
         .then((response) => {
-            res.status(200).json(response.data);
+            res.status(200).json(response.data);            
         })
-        .catch((err) => {
+        .catch((err) => {            
+            //Verificar si se puede obtener el codigo de error 4xx,5xx desde la api de la NASA  (response.status)
             res.status(406).json({
                 msg: 'Al parecer las fechas ingresadas no dan un resultado aceptado. \
                 Por favor asegúrese de respetar el formato: YYYY-MM-DD. \
